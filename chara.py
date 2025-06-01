5e48f8624a7b98859ac14d8f6b0a550eff372d63
Altrian <altrianzero@gmail.com>
2025-06-01 16:01:13 -0300

    code optimization and Amiya handling

--- chara.py
+++ chara.py
@@ -9,6 +9,10 @@ pp = pprint.PrettyPrinter(indent=4)
 STAT_CONVERT = {'maxHp': "hp", "magicResistance": "res", "attackSpeed": "aspd",
                 "moveSpeed": "ms", "respawnTime": "respawnTime", "atk": 'atk', "def": "def", "cost": "cost"}
 
+ATTRIBUTE_TRANSLATE_TABLE = {'COST': "cost", "RESPAWN_TIME": 'respawnTime', 'ATK': "atk",
+                             "MAX_HP": "hp", "ATTACK_SPEED": "aspd", "DEF": "def", "MAGIC_RESISTANCE": "res"}
+
+
 script_dir = os.path.dirname(__file__)  # <-- Absolute directory for the script
 
 cn_char_table_path = os.path.join(
@@ -67,12 +71,14 @@ filtered_cn_char_table = {key: cn_char_table[key] for key in cn_char_table.keys(
 subProfessionIds = []
 
 for id, character_dict in filtered_cn_char_table.items():
+    in_global = id in en_char_table
     if character_dict['subProfessionId'] not in subProfessionIds:
         subProfessionIds.append(character_dict['subProfessionId'])
     skills = []
     talents = []
     for skill in character_dict['skills']:
-        blackboard = chara_skills.get(skill['skillId'], {}).get('blackboard', [])
+        blackboard = chara_skills[skill['skillId']
+                                  ]['blackboard'] if skill['skillId'] in chara_skills else []
         skills.append({"skillId": skill['skillId'],
                        "name_zh": chara_skills[skill['skillId']]['name_zh'],
                        "name_ja": chara_skills[skill['skillId']]['name_ja'],
@@ -81,7 +87,6 @@ for id, character_dict in filtered_cn_char_table.items():
                        "durationType": chara_skills[skill['skillId']]['durationType'],
                        "spType": chara_skills[skill['skillId']]['spType'],
                        "levels": chara_skills[skill['skillId']]['levels'],
-                       "tags": chara_skills[skill['skillId']]['tags'],
                        "blackboard": blackboard,})
 
     if character_dict['talents']:
@@ -94,62 +99,80 @@ for id, character_dict in filtered_cn_char_table.items():
                 "prefabKey": maxed_talent['prefabKey'],
                 "name_zh": maxed_talent["name"], "name_ja": "", "name_en": "",
                 "desc_zh": chara_talents[id]['talents'][talent_index]["desc_zh"], "desc_ja": "", "desc_en": ""}
-            if id in en_char_table:
+            if id in in_global:
                 talent_dict["name_ja"] = jp_char_table[id]['talents'][talent_index]['candidates'][max_candidate_index]["name"]
                 talent_dict["desc_ja"] = chara_talents[id]['talents'][talent_index]["desc_ja"]
                 talent_dict["name_en"] = en_char_table[id]['talents'][talent_index]['candidates'][max_candidate_index]["name"]
                 talent_dict["desc_en"] = chara_talents[id]['talents'][talent_index]["desc_en"]
-            talent_dict['rangeId'] = chara_talents[id]['talents'][talent_index].get('rangeId', None)
-            talent_dict['tags'] = chara_talents[id]['talents'][talent_index].get('tags', [])
-            talent_dict['blackboard'] = chara_talents[id]['talents'][talent_index].get('blackboard', [])
+            if id in chara_talents:
+                talent_dict['rangeId'] = chara_talents[id]['talents'][talent_index].get('rangeId', None)
+                talent_dict['blackboard'] = chara_talents[id]['talents'][talent_index].get('blackboard', [])
             talents.append(talent_dict)
 
     uniequip_list = []
     for equip_id in uniequip_dict:
         if uniequip_dict[equip_id]['charId'] == id:
             uniequip_list.append(uniequip_dict[equip_id])
-    uniequip_list.sort(key=lambda x: x['order'])
+    uniequip_list.sort(key=lambda x: x['uniEquipId'])
 
-    stats = {}
-    stats['rangeId'] = character_dict['phases'][-1]['rangeId']
-    stats['level'] = character_dict['phases'][-1]['attributesKeyFrames'][-1]['level']
-    stats['hp'] = character_dict['phases'][-1]['attributesKeyFrames'][-1]['data']["maxHp"]
-    stats['atk'] = character_dict['phases'][-1]['attributesKeyFrames'][-1]['data']["atk"]
-    stats['def'] = character_dict['phases'][-1]['attributesKeyFrames'][-1]['data']["def"]
-    stats['res'] = character_dict['phases'][-1]['attributesKeyFrames'][-1]['data']["magicResistance"]
-    stats['cost'] = character_dict['phases'][-1]['attributesKeyFrames'][-1]['data']["cost"]
-    stats['blockCnt'] = character_dict['phases'][-1]['attributesKeyFrames'][-1]['data']["blockCnt"]
-    stats['aspd'] = character_dict['phases'][-1]['attributesKeyFrames'][-1]['data']["baseAttackTime"]
-    stats['respawnTime'] = character_dict['phases'][-1]['attributesKeyFrames'][-1]['data']["respawnTime"]
+    # Handle stats
+    stats_list = []
+    for phase_index, phase in enumerate(character_dict['phases']):
+        if phase_index == 0:
+            attributes = phase['attributesKeyFrames'][0]['data']
+            stats = {}
+            stats['rangeId'] = phase['rangeId']
+            stats['level'] = 1
+            stats['hp'] = attributes["maxHp"]
+            stats['atk'] = attributes["atk"]
+            stats['def'] = attributes["def"]
+            stats['res'] = attributes["magicResistance"]
+            stats['cost'] = attributes["cost"]
+            stats['blockCnt'] = attributes["blockCnt"]
+            stats['aspd'] = attributes["baseAttackTime"]
+            stats['respawnTime'] = attributes["respawnTime"]
+            stats_list.append(stats)
+        
+        attributes = phase['attributesKeyFrames'][1]['data']
+        stats = {}
+        stats['rangeId'] = phase['rangeId']
+        stats['level'] = phase['maxLevel']
+        stats['hp'] = attributes["maxHp"]
+        stats['atk'] = attributes["atk"]
+        stats['def'] = attributes["def"]
+        stats['res'] = attributes["magicResistance"]
+        stats['cost'] = attributes["cost"]
+        stats['blockCnt'] = attributes["blockCnt"]
+        stats['aspd'] = attributes["baseAttackTime"]
+        stats['respawnTime'] = attributes["respawnTime"]
+        stats_list.append(stats)
 
     # Handle potential attributes
-    potential = []
-    attribute_translate_table = {'COST': "cost", "RESPAWN_TIME": 'respawnTime', 'ATK': "atk",
-                                 "MAX_HP": "hp", "ATTACK_SPEED": "aspd", "DEF": "def", "MAGIC_RESISTANCE": "res"}
-
+    potential_list = []
     for potential_index, potential in enumerate(character_dict['potentialRanks']):
         potential_dict = {
             "desc_zh": potential['description'],
             "desc_ja": "",
             "desc_en": ""
         }
-        if id in jp_char_table and potential_index < len(jp_char_table[id]['potentialRanks']):
+        
+        if id in in_global and potential_index < len(jp_char_table[id]['potentialRanks']):
             potential_dict["desc_ja"] = jp_char_table[id]['potentialRanks'][potential_index]['description']
             potential_dict["desc_en"] = en_char_table[id]['potentialRanks'][potential_index]['description']
             
         attribute = None
         if potential.get("buff"):
             modifier = potential['buff']['attributes']['attributeModifiers'][0]
-            attribute = {attribute_translate_table[modifier['attributeType']]: modifier['value']}
+            attribute = {ATTRIBUTE_TRANSLATE_TABLE[modifier['attributeType']]: modifier['value']}
         potential_dict['attribute'] = attribute
-        potential.append(potential_dict)
+        potential_list.append(potential_dict)
 
     # Handle trust/favor
     favor_data = {}
     if character_dict['favorKeyFrames'] is not None:
         favor_last = character_dict['favorKeyFrames'][-1]['data']
-        for stat, value in favor_last:
-            if stat in STAT_CONVERT:
+        for stat, value in favor_last.items():
+            if stat in STAT_CONVERT and bool(value):
                 favor_data[STAT_CONVERT[stat]] = value
     
     # Handle tokens
@@ -183,10 +206,10 @@ for id, character_dict in filtered_cn_char_table.items():
                    "releaseDate": imple_dates.get(id, 0), "powers": powers,
                    "position": character_dict['position'], "isSpChar": character_dict['isSpChar'],
                    "rarity": character_dict['rarity'], "profession": character_dict['profession'], "subProfessionId": character_dict['subProfessionId'],
-                   "stats": stats, "potential": potential, "favorData": favor_data, "tokens": tokens,
+                   "stats": stats_list, "potential": potential_list, "favorData": favor_data, "tokens": tokens,
                    "skills": skills, "talents": talents, "uniequip": uniequip_list}
     
-    if id in jp_char_table:
+    if id in in_global:
         desc_ja = jp_char_table[id]['description'].replace("<$ba", "<ba")
         desc_en = en_char_table[id]['description'].replace("<$ba", "<ba")
         if character_dict['subProfessionId'] in ['librator', 'healer', 'musha']:
@@ -203,3 +226,141 @@ for id, character_dict in filtered_cn_char_table.items():
         result_dict["desc_ja"] = desc_ja
         result_dict["desc_en"] = desc_en
     data.append(result_dict)
+
+# Patch table for Amiya
+for id, character_dict in cn_patch_table['patchChars'].items():
+    in_global = id in en_patch_table['patchChars']
+    skills = []
+    talents = []
+    for skill in character_dict['skills']:
+        blackboard = chara_skills[skill['skillId']
+                                  ]['blackboard'] if skill['skillId'] in chara_skills else []
+        levels = chara_skills[skill['skillId']]['levels']
+        for level in levels:
+            del level['blackboard']
+        skills.append({"skillId": skill['skillId'],
+                       "name_zh": chara_skills[skill['skillId']]['name_zh'],
+                       "name_ja": chara_skills[skill['skillId']]['name_ja'],
+                       "name_en": chara_skills[skill['skillId']]['name_en'],
+                       "skillType": chara_skills[skill['skillId']]['skillType'],
+                       "durationType": chara_skills[skill['skillId']]['durationType'],
+                       'spType': chara_skills[skill['skillId']]['spType'],
+                       "levels": levels,
+                       "tags": chara_skills[skill['skillId']]['tags'] if skill['skillId'] in chara_skills else [],
+                       "blackboard": blackboard})
+
+    if character_dict['talents']:
+        for talent_index, talent in enumerate(character_dict['talents']):
+            max_candidate_index = len(talent['candidates']) - 1
+            maxed_talent = talent['candidates'][max_candidate_index]
+            talent_dict = {
+                "prefabKey": maxed_talent['prefabKey'], "name_zh": maxed_talent["name"], "name_ja": "", "name_en": "",
+                "desc_zh": chara_talents[id]['talents'][talent_index]["desc_zh"], "desc_ja": "", "desc_en": ""}
+            if in_global:
+                talent_dict["name_ja"] = jp_patch_table['patchChars'][id]['talents'][talent_index]['candidates'][max_candidate_index]["name"]
+                talent_dict["desc_ja"] = chara_talents[id]['talents'][talent_index]["desc_ja"]
+                talent_dict["name_en"] = en_patch_table['patchChars'][id]['talents'][talent_index]['candidates'][max_candidate_index]["name"]
+                talent_dict["desc_en"] = chara_talents[id]['talents'][talent_index]["desc_en"]
+            talent_dict['blackboard'] = chara_talents[id]['talents'][talent_index]['blackboard'] if id in chara_talents else []
+            talents.append(talent_dict)
+
+    uniequip_list = []
+    for equip_id in uniequip_dict:
+        if uniequip_dict[equip_id]['charId'] == id:
+            uniequip_list.append(uniequip_dict[equip_id])
+    uniequip_list.sort(key=lambda x: x['uniEquipId'])
+
+    # Handle stats
+    stats_list = []
+    for phase_index, phase in enumerate(character_dict['phases']):
+        if phase_index == 0:
+            attributes = phase['attributesKeyFrames'][0]['data']
+            stats = {}
+            stats['rangeId'] = phase['rangeId']
+            stats['level'] = 1
+            stats['hp'] = attributes["maxHp"]
+            stats['atk'] = attributes["atk"]
+            stats['def'] = attributes["def"]
+            stats['res'] = attributes["magicResistance"]
+            stats['cost'] = attributes["cost"]
+            stats['blockCnt'] = attributes["blockCnt"]
+            stats['aspd'] = attributes["baseAttackTime"]
+            stats['respawnTime'] = attributes["respawnTime"]
+            stats_list.append(stats)
+        
+        attributes = phase['attributesKeyFrames'][1]['data']
+        stats = {}
+        stats['rangeId'] = phase['rangeId']
+        stats['level'] = phase['maxLevel']
+        stats['hp'] = attributes["maxHp"]
+        stats['atk'] = attributes["atk"]
+        stats['def'] = attributes["def"]
+        stats['res'] = attributes["magicResistance"]
+        stats['cost'] = attributes["cost"]
+        stats['blockCnt'] = attributes["blockCnt"]
+        stats['aspd'] = attributes["baseAttackTime"]
+        stats['respawnTime'] = attributes["respawnTime"]
+        stats_list.append(stats)
+
+    # Handle potential attributes
+    potential_list = []
+    for potential_index, potential in enumerate(character_dict['potentialRanks']):
+        potential_dict = {
+            "desc_zh": potential['description'],
+            "desc_ja": "",
+            "desc_en": ""
+        }
+        if id in jp_char_table and potential_index < len(jp_char_table[id]['potentialRanks']):
+            potential_dict["desc_ja"] = jp_char_table[id]['potentialRanks'][potential_index]['description']
+            potential_dict["desc_en"] = en_char_table[id]['potentialRanks'][potential_index]['description']
+            
+        attribute = None
+        if potential.get("buff"):
+            modifier = potential['buff']['attributes']['attributeModifiers'][0]
+            attribute = {ATTRIBUTE_TRANSLATE_TABLE[modifier['attributeType']]: modifier['value']}
+        potential_dict['attribute'] = attribute
+        potential_list.append(potential_dict)
+
+    # Handle trust/favor
+    favor_data = {}
+    if character_dict['favorKeyFrames'] is not None:
+        favor_last = character_dict['favorKeyFrames'][-1]['data']
+        for stat, value in favor_last.items():
+            if stat in STAT_CONVERT and bool(value):
+                favor_data[STAT_CONVERT[stat]] = value
+
+    # Handle tokens
+    tokens = []
+    if character_dict['displayTokenDict'] is not None:
+        tokens = [tokens_dict[key] for key in character_dict['displayTokenDict']]
+
+    # Handle nationality and affiliations
+    powers = []
+    powers_list = character_dict['subPower'] or []
+    powers_list.append(character_dict['mainPower'])
+    for power in powers_list:
+        if power['nationId'] is not None:
+            powers.append(power['nationId'])
+        if power['groupId'] is not None: 
+            powers.append(power['groupId'])
+        if power['teamId'] is not None: 
+            powers.append(power['teamId'])
+
+    result_dict = {"id": id, "appellation": character_dict['appellation'],
+                   "name_zh": character_dict['name'], "name_ja": "", "name_en": "",
+                   "desc_zh": desc_zh, "desc_ja": "", "desc_en": "",
+                   "releaseDate": imple_dates.get(id, 0), "powers": powers,
+                   "position": character_dict['position'], "isSpChar": character_dict['isSpChar'],
+                   "rarity": character_dict['rarity'], "profession": character_dict['profession'], "subProfessionId": character_dict['subProfessionId'],
+                   "stats": stats_list, "potential": potential_list, "favorData": favor_data, "tokens": tokens,
+                   "skills": skills, "talents": talents, "uniequip": uniequip_list}
+    
+    if in_global:
+        result_dict["name_ja"] = jp_patch_table['patchChars'][id]['name']
+        result_dict["name_en"] = en_patch_table['patchChars'][id]['name']
+        result_dict["desc_ja"] = jp_patch_table['patchChars'][id]['description'].replace("<$ba", "<ba")
+        result_dict["desc_en"] = en_patch_table['patchChars'][id]['description'].replace("<$ba", "<ba")
+    data.append(result_dict)
+
+with open('characters_read.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
